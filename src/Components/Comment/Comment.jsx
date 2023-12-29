655877acdab352cdc2d2d7027da7b54b646b9540
Isabelly <isabellyd429@gmail.com>
2023-12-29 11:55:38 -0300

    :sparkles::construction: Componente de comentário

---
+++ src/Components/Comment/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+export function Comment(){
+
+    return(
+        <div className={styles.comment}>
+            <img src=" https://avatars.githubusercontent.com/u/94193803?v=4"/>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Isabelly dos Santos</strong>
+                            <time title='29 de dezembro de 2023' dateTime='2023-12-29'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}

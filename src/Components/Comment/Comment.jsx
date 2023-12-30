3c25fa021e8ba3931435cc69a2098a42cdf4fc00
Isabelly <isabellyd429@gmail.com>
2023-12-30 00:08:20 -0300

    :sparkles: Realizando likes

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from '../Avatar/Avatar'
+import { useState } from 'react'
 export function Comment({ content, deleteComment }) {
+
+    const [likeCount, setLikeCount] = useState(0);
 
     function handleDeleteComment() {
         deleteComment(content)
+    }
+
+    function handleLikeComment(){
+        setLikeCount(likeCount + 1);
     }
 
     return (
@@ -26,9 +33,9 @@ export function Comment({ content, deleteComment }) {
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
